3664681a868ff6249250c0061cca7dd79756e8f6
Yevhenii Baranik <baranik18@gmail.com>
2024-07-18 23:07:05 +0100

    init

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,15 @@
 function filterArray(numbers, value) {
 
-const filteredArray = numbers.filter(function (number) {
-		return number > value;
-	});
+	const filteredArray = [];
+	for (let i = 0; i < numbers.length; i++) {
+		if (numbers[i] >= value) {
+			filteredArray.push(numbers[i]);
+
+		}
+	}
+
 	return filteredArray;
+
 }
 
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
